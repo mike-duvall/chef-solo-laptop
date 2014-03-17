138f6704d93c1d7bbd1e8cc9fb842eca70288006
mduvall <mduvall@mint-barney.(none)>
2014-03-16 20:41:35 -0500

    Cleaned up

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ chef-solo-laptop
 ================
 
 Chef Solo project to provision my Linux dev laptop
+
+To use do:
+
+sudo su -
+git clone https://github.com/mike-duvall/chef-solo-laptop.git
+cd chef-solo-laptop
+./install.sh
+
+
+
+
+
--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #Run all as root (sudo su - )
 
-
-#yes | apt-get purge medibuntu-keyring
-#rm /etc/apt/sources.list.d/medibuntu.list
+# This is for Linux Mint 13 LTS
+# Remove obsolete medibuntu references
 sed -i '/^deb http:\/\/packages.medibuntu.org*/d' /etc/apt/sources.list /etc/apt/sources.list.d/*.list
+
+# Do an update
+# Hard coded as shell command for now.  Consider shifting to chef later
 apt-get update
-yes | apt-get upgrade
+yes | apt-get dist-upgrade
+
+# Install chef
 apt-get install curl
 curl -L https://www.opscode.com/chef/install.sh | bash
+
+# Run chef-solo
 chef-solo -c solo.rb -j solo.json
 
